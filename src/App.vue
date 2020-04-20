37b9d2df8fe9c91a6d77baaccad4e6c027a19fd0
徐毅 <xuyi@xuyideMacBook-Pro.local>
2020-04-20 22:01:39 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+    <main-tar-bar></main-tar-bar>
+  </div>
+</template>
+
+<script>
+  import MainTarBar from "./components/content/maintarbar/MainTarBar";
+  export default {
+    components:{
+      MainTarBar
+    }
+  }
+</script>
+
+<style>
+</style>
